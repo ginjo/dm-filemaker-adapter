657c527a8f7ebc7ab52d1d2cfa4855b40e6d3473
wbr <wbr@mac.com>
2015-03-02 04:07:34 -0800

    Improving specs for to_fmp_query.

--- spec/dm-filemaker-adapter/adapter_spec.rb
+++ spec/dm-filemaker-adapter/adapter_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
+# Rspec Issues (maybe not bugs, but certainly not expected behavior!).
+#
+# expect/allow_any_instance_of does not work with block form of arguments in '...to receive(:message) do |args|
+# The args will always just be the instance object.
+#
+# and_call_original breaks block form of expect.
+#
+
 describe DataMapper do
 	before :each do
 		DataMapper.setup(:default, 'filemaker://user:pass@hostname.com/DatabaseName')
@@ -64,18 +72,51 @@ describe DataMapper do
 	end # datamapper-adapters-filemaker
 	
 	describe DataMapper::Query do
+
+		# RSpec.shared_examples "#to_fmp_query" do
+		# 	it 'Receives dm query conditions and returns fmp query' do
+		# 		allow_any_instance_of(Rfm::Layout).to receive(:find).and_return(Rfm::Resultset.allocate)
+		# 		expect_any_instance_of(DataMapper::Query).to receive(:to_fmp_query).at_least(:once).and_call_original
+		# 		query.inspect
+		# 	end
+		# end
+
 	  
 	  describe '#to_fmp_query' do
-	  	before(:each){allow_any_instance_of(Rfm::Layout).to receive(:find).and_return(Rfm::Resultset.allocate)}
-	  
-	  	it 'Receives dm query conditions and returns fmp query' do
-				expect_any_instance_of(DataMapper::Query).to receive(:to_fmp_query).at_least(:once) do |conditions|
-					expect(conditions.class.ancestors.include?(DataMapper::Query::Conditions::AbstractOperation)).to eq(true)
-				end.and_call_original
+	  	before(:each) do
+				allow_any_instance_of(Rfm::Layout).to receive(:find).and_return(Rfm::Resultset.allocate)
+				#expect_any_instance_of(DataMapper::Query).to receive(:to_fmp_query).at_least(:once).and_call_original
+			end
+			
+			it 'returns bla bla with a simple .all query' do
 				User.all(:id=>1).inspect
-				#puts User.first(:id=>1).inspect
-				#puts (User.all(:id=>1) | User.all(:id=>2)).inspect
-	  	end
+			end
+			
+			it 'this is the right spec to use - it will allow testing query object & return value' do
+				expect(User.repository.adapter).to receive(:read) do |query|
+					puts "QUERY #{query}"
+					expect(query.to_fmp_query.class).to eq(Hash)
+					[]
+				end
+				User.all(:id=>1).inspect
+			end
+	  
+			# it 'Receives dm query conditions and returns fmp query' do
+			# 	expect_any_instance_of(DataMapper::Query).to receive(:to_fmp_query).at_least(:once) do |conditions|
+			#			# This won't get called here, if you have the and_call_original. I've tried every which way to get it to work.
+			# 		expect(conditions.class.ancestors.include?(DataMapper::Query::Conditions::AbstractOperation)).to eq(true)
+			# 	end.and_call_original
+			# 	User.all(:id=>1).inspect
+			# 	#User.first(:id=>1).inspect
+			# 	#(User.all(:id=>1) | User.all(:id=>2)).inspect
+			# end
+			
+			# context "with simple .all query" do
+			# 	include_examples "#to_fmp_query" do
+			# 		let(:query){User.all(:id=>1)}
+			# 	end
+			# end
+
 	
 	  end	#to_fmp_query
 	
